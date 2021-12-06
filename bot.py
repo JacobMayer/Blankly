b2b3702f1112e277a0ebf9b44367f6605d44aa37
JacobMayer <64308138+JacobMayer@users.noreply.github.com>
2021-12-06 15:30:08 -0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from blankly import Strategy, StrategyState, Interface
 from blankly import Alpaca
 from blankly.utils import trunc
-from blankly.indicators import macd
+from blankly.indicators import macd, sma, rsi
+
 
 SHORT_PERIOD = 12
 LONG_PERIOD = 26
@@ -22,12 +23,9 @@ def init(symbol, state: StrategyState):
     variables['has_bought'] = False
 
 
-def price_event(price, symbol, state: StrategyState):
-    interface: Interface = state.interface
-    # allow the resolution to be any resolution: 15m, 30m, 1d, etc.
-    variables = state.variables
-
-    variables['history'].append(price)
+def macd_strat(var, inter, symbol, price):
+    variables = var
+    interface = inter
     macd_res, macd_signal, macd_histogram = macd(variables['history'], 
                                                  short_period=variables['short_period'],
                                                  long_period=variables['long_period'],
@@ -51,10 +49,59 @@ def price_event(price, symbol, state: StrategyState):
         interface.market_order(symbol, 'sell', int(interface.account[symbol].available))
         variables['has_bought'] = False
 
+def golden_cross(var, inter, symbol, price):
+    variables = var
+    interface = inter
+    
+    sma200 = sma(variables['history'], period=200)
+    # match up dimensions
+    sma50 = sma(variables['history'], period=50)[-len(sma200):]
+    diff = sma200 - sma50
+    slope_sma50 = (sma50[-1] - sma50[-5]) / 5 # get the slope of the last 5 SMA50 Data Points
+    prev_diff = diff[-2]
+    curr_diff = diff[-1]
+    is_cross_up = slope_sma50 > 0 and curr_diff >= 0 and prev_diff < 0
+    is_cross_down = slope_sma50 < 0 and curr_diff <= 0 and prev_diff > 0
+    # comparing prev diff with current diff will show a cross
+    if is_cross_up and not variables['has_bought']:
+        interface.market_order(symbol, 'buy', int(interface.cash/price))
+        variables['has_bought'] = True
+    elif is_cross_down and variables['has_bought']:
+        # use strategy.base_asset if on CoinbasePro or Binance
+        # truncate here to fix any floating point errors
+        interface.market_order(symbol, 'sell', int(interface.account[symbol].available))
+        variables['has_bought'] = False
+    
+def rsi(var, inter, symbol, price):
+    variables = var
+    interface = inter
+    rsi = blankly.indicators.rsi(state.variables['history'])
+    if rsi[-1] < 30 and not state.variables['owns_position']:
+        # Dollar cost average buy
+        buy = int(state.interface.cash/price)
+        state.interface.market_order(symbol, side='buy', size=buy)
+        state.variables['owns_position'] = True
+    elif rsi[-1] > 70 and state.variables['owns_position']:
+        # Dollar cost average sell
+        curr_value = int(state.interface.account[state.base_asset].available)
+        state.interface.market_order(symbol, side='sell', size=curr_value)
+        state.variables['owns_position'] = False
+    
+def price_event(price, symbol, state: StrategyState):
+    interface: Interface = state.interface
+    # allow the resolution to be any resolution: 15m, 30m, 1d, etc.
+    variables = state.variables
+
+    variables['history'].append(price)
+    
+ #   macd_strat(variables, interface, symbol, price)
+ #   golden_cross(variables, interface, symbol, price)
+ #   rsi(variables, interface, symbol, price)
 
 alpaca = Alpaca()
 s = Strategy(alpaca)
-s.add_price_event(price_event, 'TSLA', resolution='1d', init=init)
+s.add_price_event(price_event, 'SPY', resolution='1d', init=init)
+
 s.backtest(initial_values={'USD': 10000}, to='2y')
 
 
