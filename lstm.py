e7879d1ea23ba1c08a056aa0534d1296a9a425f9
Zim Agu <zimmagu@gmail.com>
2021-12-09 13:47:38 -0800

    Was able to successfully calculate root mean squared error and mean absolute percentage error

--- lstm.py
+++ lstm.py
@@ -14,6 +14,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 # import tensorflow
 from pandas_datareader.data import DataReader
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
 
 
 # Streamlit Imports
@@ -40,7 +43,7 @@ stockDataFrame = stockDataFrame.append(stock ,sort=False) # Adds the stock price
 stockDataFrame['Symbol'] = ticker
 
 
-
+print("Hello")
 
 
 sns.set_style("whitegrid") # Sets style for plotting
@@ -59,6 +62,107 @@ stockDataFrame["Daily Return"] = stockDataFrame["Adj Close"].pct_change()
 sns.displot(stockDataFrame["Daily Return"].dropna(), bins=100, color="goldenrod") # Shows whether or not the daily return is evenly distributed
 # plt.title("Daily Return")
 # plt.show()
+
+
+
+
+
+# Creating a test and data set
+data = stockDataFrame.filter(["Close"]) # Creating a new dataframe with the close column
+dataset = data.values # Converts the dataframe to a numpy array
+
+
+trainingDataLength = int(np.ceil( len(dataset) * .95)) # Gets the number of rows to train the model on
+# print(trainingDataLength)
+
+
+
+# Creating the scaled data set
+scaler = MinMaxScaler(feature_range=(0,1))
+scaledData = scaler.fit_transform(dataset)
+# print(scaledData)
+
+
+# Creating the training data set
+trainingData = scaledData[0:int(trainingDataLength), :]
+
+
+# Splits the training data into two seperate data sets
+xTrainingData = []
+yTrainingData = []
+
+
+for i in range (60, len(trainingData)): # This for loop fills the x and y training data sets
+    xTrainingData.append(trainingData[i - 60:i, 0])
+    yTrainingData.append(trainingData[i, 0])
+
+    # if i <= 61:
+    #     print(xTrainingData)
+    #     print(yTrainingData)
+    #     print(" ")
+
+
+# Converting the x training data and the y training data to numpy arrays
+xTrainingData = np.array(xTrainingData)
+yTrainingData = np.array(yTrainingData)
+
+
+xTrainingData = np.reshape(xTrainingData, (xTrainingData.shape[0], xTrainingData.shape[1], 1))
+
+
+
+
+
+# Building the LSTM (Long Short Term Memory) Model
+lstm_Model = Sequential() # provides training and inference features on this model.
+lstm_Model.add(LSTM(128, return_sequences=True, input_shape=(xTrainingData.shape[1], 1))) # model.add Adds a layer instance on top of the layer stack
+lstm_Model.add(LSTM(64, return_sequences=False))
+lstm_Model.add(Dense(25))
+lstm_Model.add(Dense(1))
+
+
+# Compiling the LSTM model
+lstm_Model.compile(optimizer="Adam", loss="mse") # configs the model with losses and metrics
+
+# Trains the lstm Model
+lstm_Model.fit(xTrainingData, yTrainingData, batch_size=1, epochs=1) # Give the model 45 or so seconds to run
+
+
+
+# Creates new testing data set
+testData = scaledData[trainingDataLength - 60: , : ]
+
+
+# Splits the training data into two seperate data sets
+xTestData = []
+yTestData = dataset[trainingDataLength:, :]
+
+
+for i in range(60, len(testData)): # Adds data to x Testing data
+    xTestData.append(testData[i - 60:i, 0])
+
+
+
+xTestData = np.array(xTestData)# Converts the testing data to a numpy array
+
+
+xTestData = np.reshape(xTestData, (xTestData.shape[0], xTestData.shape[1], 1)) # Reshapes the x testing data
+
+
+
+# Gets the LSTM model's predicted price values
+predictions = lstm_Model.predict(xTestData)
+predictions = scaler.inverse_transform(predictions)
+
+
+# Calculates the root mean squared error
+rootMeanSquaredError = np.sqrt(np.mean(((predictions - yTestData) ** 2)))
+print('rmse ', rootMeanSquaredError)
+
+# Calculates the mean absolute percentage error
+meanAbsolutePercentageError = np.mean(np.abs((yTestData - predictions) / yTestData)) * 100
+print('mape ', meanAbsolutePercentageError)
+
 
 # Mat plot lib plot
 # plt.figure(figsize=(13,4))
