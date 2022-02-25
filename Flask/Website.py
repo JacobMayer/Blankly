96ac024f0a2f8a2118d9cda8027efd79b9d2edbe
Justin Carlson <41803725+justincarlson32@users.noreply.github.com>
2022-02-24 22:25:02 -0800

    Update Website.py

--- Flask/Website.py
+++ Flask/Website.py
@@ -2,6 +2,11 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+@app.route('/assets/<path:path>')
+def asset(path):
+    print(path)
+    return send_from_directory('assets', path)
+
 @app.route("/")
 def home():
     return render_template("child.html")
